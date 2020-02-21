071e3c079018bbfde09fa268fbbc192eb7accdaf
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2020-02-21 09:37:21 +0100

    c

--- demo/src/main/java/com/tpmil/demo/config/SpringSecurityConfig.java
+++ demo/src/main/java/com/tpmil/demo/config/SpringSecurityConfig.java
@@ -1,24 +1,26 @@
 package com.tpmil.demo.config;
 
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.AuthenticationEntryPoint;
 
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+// @Configuration
+// @EnableWebSecurity
+public class SpringSecurityConfig 
+// extends WebSecurityConfigurerAdapter 
+{
 
-	@Autowired
-	private AuthenticationEntryPoint authEntryPoint;
+// 	@Autowired
+// 	private AuthenticationEntryPoint authEntryPoint;
 		
 	// @Override
 	// protected void configure(HttpSecurity http) throws Exception {
